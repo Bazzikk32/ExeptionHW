3740c07bf9f5647be047978f6c8edb76c4f55b5c
Bazzikk <bazanmikha@googlemail.com>
2024-08-14 23:31:51 +0300

    Add final corrections

--- src/Service.java
+++ src/Service.java
@@ -87,7 +87,8 @@ public class Service {
 
     public static boolean isValidDate(String birtDay)
     {
-        String regex = "^(1[0-2]|0[1-9]).(3[01]" + "|[12][0-9]|0[1-9]).[0-9]{4}$";
+        //String regex = "^(1[0-2]|0[1-9]).(3[01]" + "|[12][0-9]|0[1-9]).[0-9]{4}$";
+        String regex = "^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher((CharSequence)birtDay);
         if (matcher.matches()){
